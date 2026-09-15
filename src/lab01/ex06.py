427c6ed77a6f0eb509c4fd2e7da3595843fe1068
Gdalevich Oleg <gdalevich.oleg@gmail.com>
2026-09-15 15:09:42 +0300

    Change ex06.py

--- src/lab01/ex06.py
+++ src/lab01/ex06.py
@@ -1,10 +1,12 @@
-n = int(input())
+count = 1
+n = int(input(f'in_{count}: '))
 
 k=0
 
 for _ in range(n):
-    sp = list(map(str, input().split()))
+    count+=1
+    sp = list(map(str, input(f'in_{count}: ').split()))
     if sp[-1] == 'True':
         k += 1
 
-print(k, n-k)
+print(f'out: {k}, {n-k}')
