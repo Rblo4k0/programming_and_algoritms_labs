5416d3b14fa3a7d407b965439141351e90fe4e85
Gdalevich Oleg <gdalevich.oleg@gmail.com>
2026-09-21 10:14:01 +0300

    Change lib

---
+++ src/lib/scripts.py
@@ -0,0 +1,17 @@
+def is_matrix_ok(mat: list[list]) -> bool:
+
+    '''Проверяет не рваная ли матрица
+
+    Args:
+        mat: Матрица
+    
+    Returns:
+        True: Матрица нерваная
+        False: Матрица рваная
+    '''
+    n, m = len(mat), len(mat[0])
+
+    for i in mat:
+        if len[i] != m:
+            return False
+    return True
